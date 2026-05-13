9b8ce8e054be83e584fc3d2cdd068f0aa1e21a82
yxljtcnq <m19880789869@163.com>
2026-05-13 13:00:05 +0800

    测试更新指向是否成功

--- daily/src/lowerpart/myio/myzipstream/ZipStreamDemo3_CompressionFolder.java
+++ daily/src/lowerpart/myio/myzipstream/ZipStreamDemo3_CompressionFolder.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+//看到这行就说更新指向成功了
+
 public class ZipStreamDemo3_CompressionFolder {
     static void main(String[] args) throws IOException {
         /*
